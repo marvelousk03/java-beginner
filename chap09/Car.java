d5894355c64d649c521541cafae140604cc6b987
marvek16 <marvelousk1603f@gmail.com>
2025-09-17 11:55:47 +0200

    commit

---
+++ chap09/Car.java
@@ -0,0 +1,15 @@
+package Chap09.Page258;
+
+public class Car {
+    public Car() {
+        // default constructor
+    }
+
+    public Car(String name) {
+        // constructor with String parameter
+    }
+
+    public Car(int size) {
+        // constructor with int parameter
+    }
+}
