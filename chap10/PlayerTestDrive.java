c518158973259853f8ad70f0185e6a687b67f453
marvek16 <marvelousk1603f@gmail.com>
2025-09-18 10:36:50 +0200

    commit

---
+++ chap10/PlayerTestDrive.java
@@ -0,0 +1,15 @@
+package chap10;
+
+public class PlayerTestDrive {
+    public static void main(String[] args) {
+        // Access static variable using class name, before any Player object is created
+        System.out.println(Player.playerCount);  // Output: 0
+
+        // Create a new Player object
+        Player one = new Player("Tiger Woods");
+
+        // Access static variable again, after creating a Player object
+        System.out.println(Player.playerCount);  // Output: 1
+    }
+}
+
