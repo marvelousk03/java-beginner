a4db29b8b5c054604ed9859a3a4129f075911aa0
marvek16 <marvelousk1603f@gmail.com>
2025-09-30 10:27:58 +0200

    commit

--- jadvanced/chap11/Jukebox2.java
+++ jadvanced/chap11/Jukebox2.java
@@ -4,11 +4,11 @@ import java.util.*;
 
 public class Jukebox2 {
     public static void main(String[] args) {
-        new Jukebox1().go();
+        new Jukebox2().go();
     }
 
     public void go() {
-        List<String> songList = MockSongs.getSongStrings();
+        List<String> songList = MockSongs2.getSongStrings();
         System.out.println(songList);
         Collections.sort(songList);
         System.out.println(songList);
