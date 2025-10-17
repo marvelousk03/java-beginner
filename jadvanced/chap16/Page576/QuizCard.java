f606f6838aae845c02e644c6acec37a80d733f46
marvek16 <marvelousk1603f@gmail.com>
2025-10-17 10:53:10 +0200

    commit

---
+++ jadvanced/chap16/Page576/QuizCard.java
@@ -0,0 +1,20 @@
+package jadvanced.chap16.Page576;
+
+public class QuizCard {
+    private String question;
+    private String answer;
+
+    public QuizCard(String q, String a) {
+        question = q;
+        answer = a;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
+
